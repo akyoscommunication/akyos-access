302eacc7b3559448bc2fdd37ca5ba9a37548f963
kevin.akyos <kevin.akyos@gmail.com>
2024-12-03 17:23:10 +0100

    fix: api

--- src/View/Blocks/FormAccess.php
+++ src/View/Blocks/FormAccess.php
@@ -4,8 +4,10 @@ namespace Akyos\Access\View\Blocks;
 
 use Akyos\Core\Classes\Block;
 use Akyos\Core\Classes\GutenbergBlock;
+use Extended\ACF\Fields\ButtonGroup;
 use Extended\ACF\Fields\Image;
 use Extended\ACF\Fields\PostObject;
+use Extended\ACF\Fields\Tab;
 use Extended\ACF\Fields\Textarea;
 
 class FormAccess extends Block
@@ -22,9 +24,17 @@ class FormAccess extends Block
     protected static function fields(): array
     {
         return [
+            Tab::make('Contenu'),
             Textarea::make('Description', 'description')->newLines('br'),
+            Textarea::make('Description courte', 'description_short')->newLines('br'),
             PostObject::make('Formulaire', 'form')->postTypes(['forminator_forms'])->format('id'),
-            Image::make('Image', 'image')->format('id')
+            Image::make('Image', 'image')->format('id'),
+            Tab::make('Options'),
+            ButtonGroup::make('Position du contenu', 'content_position')
+                ->choices([
+                    'content-image' => 'Contenu / Image',
+                    'image-content' => 'Image / Contenu',
+                ])->default('content-image')
         ];
     }
 
